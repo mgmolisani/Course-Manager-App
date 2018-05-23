6fa9e7df4850f3efc859913aa830cd0e5c26c136
mgmolisani <molisani.m@husky.neu.edu>
2018-05-23 01:20:33 -0400

    Commented out the wrong main. Correcting

--- src/main/java/com/example/myapp/DemoApplication.java
+++ src/main/java/com/example/myapp/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.myapp;
+/*package com.example.myapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,4 +9,4 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-}
+}*/
--- src/main/java/webdev/CourseManagerApp.java
+++ src/main/java/webdev/CourseManagerApp.java
@@ -1,4 +1,4 @@
-/*package webdev;
+package webdev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,4 +9,4 @@ public class CourseManagerApp {
   public static void main(String[] args) {
     SpringApplication.run(CourseManagerApp.class, args);
   }
-}*/
+}
