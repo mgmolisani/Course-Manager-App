7089de71372b5faa481aa8e69ace207f16fe8cc3
mgmolisani <molisani.m@husky.neu.edu>
2018-05-24 21:05:47 -0400

    added services for password recovery

--- src/main/webapp/jquery/services/user.service.client.js
+++ src/main/webapp/jquery/services/user.service.client.js
@@ -8,13 +8,22 @@ function UserServiceClient() {
     this.register = register;
     this.login = login;
     this.updateProfile = updateProfile;
+    this.forgotPassword = forgotPassword;
+    this.updatePassword = updatePassword;
     this.userUrl = '/api/user';
     this.registerUrl = '/api/register';
     this.loginUrl = '/api/login';
     this.profileUrl = '/api/profile';
+    this.passwordUrl = '/api/password/';
 
     var self = this;
 
+    /**
+     * Sends a create user request
+     * @param user the user to create
+     * @param callback
+     * @returns {Promise<Response>}
+     */
     function createUser(user, callback) {
         return fetch(self.userUrl, {
             method: 'POST',
@@ -31,6 +40,11 @@ function UserServiceClient() {
         });
     }
 
+    /**
+     * Sends a findAllUsers request
+     * @param callback
+     * @returns {Promise<Response>}
+     */
     function findAllUsers(callback) {
         return fetch(self.userUrl)
             .then(function (response) {
@@ -42,6 +56,12 @@ function UserServiceClient() {
             });
     }
 
+    /**
+     * Sends a findUserById request
+     * @param userId the user ID
+     * @param callback
+     * @returns {Promise<Response>}
+     */
     function findUserById(userId, callback) {
         return fetch(self.userUrl + '/' + userId)
             .then(function (response) {
@@ -53,6 +73,12 @@ function UserServiceClient() {
             });
     }
 
+    /**
+     * Sends a findUserByUsername request
+     * @param username the username of the user to find
+     * @param callback
+     * @returns {Promise<Response>}
+     */
     function findUserByUsername(username, callback) {
         return fetch(self.userUrl + '?' + username)
             .then(function (response) {
@@ -64,6 +90,13 @@ function UserServiceClient() {
             });
     }
 
+    /**
+     * Sends an updateUser request
+     * @param userId the user ID to update
+     * @param user the information to use to update the user
+     * @param callback
+     * @returns {Promise<Response>}
+     */
     function updateUser(userId, user, callback) {
         return fetch(self.userUrl + '/' + userId, {
                                                       method: 'PUT',
@@ -83,6 +116,13 @@ function UserServiceClient() {
             });
     }
 
+    /**
+     * Sends an updateProfile request
+     * @param userId the user ID to update
+     * @param user the information to use to update the user
+     * @param callback
+     * @returns {Promise<Response>}
+     */
     function updateProfile(userId, user, callback) {
         return fetch(self.profileUrl + '/' + userId, {
                                                          method: 'PUT',
@@ -102,12 +142,30 @@ function UserServiceClient() {
             });
     }
 
+    /**
+     * Sends a deleteUser request
+     * @param userId the user ID to delete
+     * @param callback
+     * @returns {Promise<Response>}
+     */
     function deleteUser(userId, callback) {
         return fetch(self.userUrl + '/' + userId, {
                                                       method: 'DELETE'
-                                                  }).then(callback);
+                                                  })
+            .then(function (response) {
+                if (response.ok) {
+                    return response.text().then(callback);
+                }
+                return response.json().then(errorCallback);
+            });
     }
 
+    /**
+     * Sends a registerUser request
+     * @param user the user to register
+     * @param callback
+     * @returns {Promise<Response>}
+     */
     function register(user, callback) {
         return fetch(self.registerUrl, {
             method: 'POST',
@@ -124,6 +182,12 @@ function UserServiceClient() {
         });
     }
 
+    /**
+     * Sends a login request
+     * @param user the user to login
+     * @param callback
+     * @returns {Promise<Response>}
+     */
     function login(user, callback) {
         return fetch(self.loginUrl, {
             method: 'POST',
@@ -140,6 +204,49 @@ function UserServiceClient() {
         });
     }
 
+    /**
+     * Send a forgotPassword request
+     * @param username
+     * @param callback
+     * @returns {Promise<Response>}
+     */
+    function forgotPassword(username, callback) {
+        return fetch(self.passwordUrl + "/" + username)
+            .then(function (response) {
+                if (response.ok) {
+                    return response.text().then(callback);
+                }
+                return response.json().then(errorCallback);
+            });
+    }
+
+    /**
+     * Sends the updatePassword request.
+     * @param userId
+     * @param password
+     * @param callback
+     * @returns {Promise<Response>}
+     */
+    function updatePassword(userId, password, callback) {
+        return fetch(self.passwordUrl + '/' + userId, {
+                                                          method: 'PUT',
+                                                          body: password,
+                                                          headers: {
+                                                              'content-type': 'text/plain'
+                                                          }
+                                                      }).then(function (response) {
+            var responseJson = response.json();
+            if (response.ok) {
+                return responseJson.then(callback);
+            }
+            return responseJson.then(errorCallback);
+        });
+    }
+
+    /**
+     * A generic error callback to handle errors sent by the middle tier responses to the requests
+     * @param data the returned error object to handle
+     */
     function errorCallback(data) {
         console.log(data);
         throw new Error(data.message);
