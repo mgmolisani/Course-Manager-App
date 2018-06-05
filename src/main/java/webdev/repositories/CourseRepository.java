17239ee5b2c8882bfced483acc795e0ea4fc7e96
mgmolisani <molisani.m@husky.neu.edu>
2018-06-04 23:04:03 -0400

    updated course

--- src/main/java/webdev/repositories/CourseRepository.java
+++ src/main/java/webdev/repositories/CourseRepository.java
@@ -1,22 +1,12 @@
 package webdev.repositories;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
 import webdev.models.Course;
 
 /**
- * Interface to represent a user repository.
+ * Interface to represent a course repository.
  */
 public interface CourseRepository extends CrudRepository<Course, Integer> {
-/*  @Query("SELECT u FROM User u WHERE u.username=:username")
-  Iterable<User> findUserByUsername(
-          @Param("username") String username);
-
-  @Query("SELECT u FROM User u WHERE u.username=:username AND u.password=:password")
-  Iterable<User> findUserByCredentials(
-          @Param("username") String username,
-          @Param("password") String password);*/
 }
 
