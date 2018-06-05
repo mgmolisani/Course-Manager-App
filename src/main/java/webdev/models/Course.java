40f578dee69c5d33d89a28824bbbd066700e4259
mgmolisani <molisani.m@husky.neu.edu>
2018-06-04 23:03:25 -0400

    updated Course

--- src/main/java/webdev/models/Course.java
+++ src/main/java/webdev/models/Course.java
@@ -1,23 +1,37 @@
 package webdev.models;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import java.util.Date;
 import java.util.List;
 
 import javax.persistence.*;
 
+/**
+ * Represents a course.
+ */
 @Entity
 public class Course {
   @Id
-  @GeneratedValue(strategy= GenerationType.IDENTITY)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private int id;
   private String title;
+  private String author;
+  @CreationTimestamp
   @Temporal(TemporalType.TIMESTAMP)
   private Date created;
+  @UpdateTimestamp
   @Temporal(TemporalType.TIMESTAMP)
   private Date modified;
+  @OneToMany(mappedBy = "course", orphanRemoval = true)
+  private List<Module> modules;
 
   public Course() {
+    super();
   }
+
+  //Start of setters and getters
 
   public int getId() {
     return id;
@@ -35,6 +49,14 @@ public class Course {
     this.title = title;
   }
 
+  public String getAuthor() {
+    return author;
+  }
+
+  public void setAuthor(String author) {
+    this.author = author;
+  }
+
   public Date getCreated() {
     return created;
   }
@@ -50,4 +72,14 @@ public class Course {
   public void setModified(Date modified) {
     this.modified = modified;
   }
+
+  public List<webdev.models.Module> getModules() {
+    return modules;
+  }
+
+  public void setModules(List<Module> modules) {
+    this.modules = modules;
+  }
+
+  //End of setters and getters
 }
