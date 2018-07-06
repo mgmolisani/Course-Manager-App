d82f31be02feba688ce17d6d75c86ad841ebc3ed
mgmolisani <molisani.m@husky.neu.edu>
2018-07-05 22:18:24 -0400

    added exam services

--- src/main/java/webdev/services/AssignmentService.java
+++ src/main/java/webdev/services/AssignmentService.java
@@ -1,8 +1,6 @@
 package webdev.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.expression.spel.ast.Assign;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +8,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -22,7 +19,6 @@ import webdev.models.Lesson;
 import webdev.models.Widget;
 import webdev.repositories.AssignmentRepository;
 import webdev.repositories.LessonRepository;
-import webdev.repositories.WidgetRepository;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -66,7 +62,7 @@ public class AssignmentService {
 
   @PostMapping("/api/lesson/{lessonId}/assignment")
   public Assignment createAssignment(@PathVariable int lessonId,
-                             @RequestBody Assignment assignment) throws LessonService.LessonNotFoundException {
+                                     @RequestBody Assignment assignment) throws LessonService.LessonNotFoundException {
     Optional<Lesson> data = lessonRepository.findById(lessonId);
     if (data.isPresent()) {
       Lesson lesson = data.get();
---
+++ src/main/java/webdev/services/ExamService.java
@@ -0,0 +1,89 @@
+package webdev.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import webdev.models.Exam;
+import webdev.models.Lesson;
+import webdev.models.Widget;
+import webdev.repositories.ExamRepository;
+import webdev.repositories.LessonRepository;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+public class ExamService {
+  @Autowired
+  ExamRepository examRepository;
+  @Autowired
+  LessonRepository lessonRepository;
+
+  @GetMapping("/api/exam")
+  public Iterable<Exam> findAllExams() {
+    return examRepository.findAll();
+  }
+
+  @GetMapping("/api/exam/{examId}")
+  public Exam findExamById(@PathVariable int examId) throws WidgetService.WidgetNotFoundException {
+    Optional<Exam> data = examRepository.findById(examId);
+    if (data.isPresent()) {
+      return data.get();
+    }
+    throw new WidgetService.WidgetNotFoundException();
+  }
+
+  @GetMapping("/api/lesson/{lessonId}/exam")
+  public List<Exam> findAllExamsForLesson(@PathVariable int lessonId) throws LessonService.LessonNotFoundException {
+    Optional<Lesson> data = lessonRepository.findById(lessonId);
+    if (data.isPresent()) {
+      Lesson lesson = data.get();
+      List<Widget> widgets = lesson.getWidgets();
+      List<Exam> assignments = new ArrayList<>();
+      for (Widget widget : widgets) {
+        String widgetType = widget.getWidgetType();
+        if (widgetType != null && widgetType.equals("exam")) {
+          assignments.add((Exam) widget);
+        }
+      }
+      return assignments;
+    }
+    throw new LessonService.LessonNotFoundException();
+  }
+
+  @PostMapping("/api/lesson/{lessonId}/exam")
+  public Exam createExam(@PathVariable int lessonId,
+                         @RequestBody Exam exam) throws LessonService.LessonNotFoundException {
+    Optional<Lesson> data = lessonRepository.findById(lessonId);
+    if (data.isPresent()) {
+      Lesson lesson = data.get();
+      exam.setLesson(lesson);
+      return examRepository.save(exam);
+    }
+    throw new LessonService.LessonNotFoundException();
+  }
+
+  @PutMapping("/api/exam/{examId}")
+  public Exam updateExam(@PathVariable int examId, @RequestBody Exam updatedExam) throws WidgetService.WidgetNotFoundException {
+    Exam exam = this.findExamById(examId);
+    exam.setTitle(updatedExam.getTitle());
+    exam.setDescription(updatedExam.getDescription());
+    exam.setPoints(updatedExam.getPoints());
+    return examRepository.save(exam);
+  }
+
+  @DeleteMapping("/api/exam/{examId}")
+  public void deleteExam(@PathVariable int examId) throws WidgetService.WidgetNotFoundException {
+    this.findExamById(examId);
+    examRepository.deleteById(examId);
+  }
+}
